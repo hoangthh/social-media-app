85b88e45977d4595fe214f7057e326552c30324d
Tran Huy Hoang <tranhoang202204@gmail.com>
2025-01-04 12:25:15 +0700

    Initialize client and server

---
+++ client/src/App.js
@@ -0,0 +1,5 @@
+function App() {
+  return <p>This is blog app</p>;
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,30 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import posts from "./routers/posts.js";
+import mongoose from "mongoose";
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+const URI =
+  "mongodb+srv://tranhoang202204:udqWWfDESrv8D7zb@cluster0.yiarj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true, limit: "30mb" }));
+app.use(cors());
+
+app.use("/", posts);
+
+mongoose
+  .connect(URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("Connected to DB");
+  })
+  .catch((err) => {
+    console.log("err", err);
+  });
+
+app.listen(PORT, () => {
+  console.log("Server is running");
+});
