9b0a413a215b0b31c6361b64a676a89ded4ee846
Tran Huy Hoang <tranhoang202204@gmail.com>
2025-01-13 03:05:50 +0700

    Create post model

--- server/models/PostModel.js
+++ server/models/PostModel.js
@@ -2,10 +2,6 @@ import mongoose from "mongoose";
 
 const schema = new mongoose.Schema(
   {
-    title: {
-      type: String,
-      require: true,
-    },
     content: {
       type: String,
       require: true,
