158898051608c5eb89f9d47b2640aa8c0e61f407
Tran Huy Hoang <tranhoang202204@gmail.com>
2025-01-19 11:15:29 +0700

    Config route frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,24 @@
 import { useDispatch } from "react-redux";
 import * as actions from "./redux/actions";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import Login from "./components/Login/Login";
+import PrivateRoutes from "./routes/privateRoutes/PrivateRoutes";
 
 function App() {
-  return <HomePage />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* Public routes */}
+        <Route path="/login" element={<Login />} />
+
+        {/* Private routes */}
+        <Route element={<PrivateRoutes />}>
+          <Route path="/" element={<HomePage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
