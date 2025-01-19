1ab704521e3c16f2a5548962d3e86432172665ce
Tran Huy Hoang <tranhoang202204@gmail.com>
2025-01-19 11:16:42 +0700

    Create call api method to fetch user

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -4,3 +4,19 @@ const URL = "http://localhost:5000";
 
 export const fetchPosts = () => axios.get(`${URL}/posts`);
 export const createPost = (payload) => axios.post(`${URL}/posts`, payload);
+export const fetchUser = async () => {
+  const res = await axios.get(`${URL}/user`, {
+    withCredentials: true,
+  });
+
+  return res;
+};
+export const reactPost = async (userId, postId, reactionType) => {
+  const res = await axios.post(`${URL}/posts/react`, {
+    userId: userId,
+    postId: postId,
+    reactionType: reactionType,
+  });
+
+  return res;
+};
