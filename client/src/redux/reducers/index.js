11b607bc3d881aed70c2365646e8fb038c277d7f
Tran Huy Hoang <tranhoang202204@gmail.com>
2025-01-23 20:29:41 +0700

    Create selector for comment modal

--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from "redux";
 import posts from "./posts";
 import modal from "./modal";
 import user from "./user";
+import commentModal from "./commentModal";
 
 export default combineReducers({
   posts,
   modal,
   user,
+  commentModal,
 });
--- client/src/redux/selectors/index.js
+++ client/src/redux/selectors/index.js
@@ -1,3 +1,4 @@
 export const postsState$ = (state) => state.posts.data;
 export const modalState$ = (state) => state.modal;
+export const commentModalState$ = (state) => state.commentModal;
 export const userState$ = (state) => state.user.data;
