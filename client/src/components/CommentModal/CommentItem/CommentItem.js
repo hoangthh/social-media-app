2c71f90b7f7d7dcaf8cdd589b205229b4e7b11f4
Tran Huy Hoang <tranhoang202204@gmail.com>
2025-02-15 23:43:01 +0700

    Custom comment

---
+++ client/src/components/CommentModal/CommentItem/CommentItem.js
@@ -0,0 +1,39 @@
+import { Avatar, Tooltip } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import "./CommentItem.scss";
+import * as api from "../../../api";
+
+export default function CommentItem({ comment }) {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const fetchUserByUserId = async () => {
+      const user = await api.fetchUserByUserId(comment.userId);
+      setUser(user);
+    };
+
+    fetchUserByUserId();
+  }, [comment.userId]);
+
+  return (
+    <div className="comment-item">
+      <Avatar src={user?.avatar} />
+      <div className="comment-item--wrapper">
+        <div className="comment-item--wrapper--info">
+          <span className="comment-item--wrapper--info__username">
+            {user?.name}
+          </span>
+          <p className="comment-item--wrapper--info__comment">
+            {comment?.comment}
+          </p>
+        </div>
+
+        <Tooltip title={comment?.createdAt} placement="top">
+          <span className="comment-item--wrapper--date">
+            {comment?.createdAt}
+          </span>
+        </Tooltip>
+      </div>
+    </div>
+  );
+}
---
+++ client/src/components/CommentModal/CommentModal.js
@@ -0,0 +1,182 @@
+import React, { useCallback, useEffect, useRef, useState } from "react";
+import "./CommentModal.scss";
+import { Avatar, Modal, styled, TextField } from "@mui/material";
+import SendRoundedIcon from "@mui/icons-material/SendRounded";
+import { useDispatch, useSelector } from "react-redux";
+import { commentModalState$, userState$ } from "../../redux/selectors";
+import * as actions from "../../redux/actions";
+import Post from "../Post/Post";
+import CommentItem from "./CommentItem/CommentItem";
+import * as api from "../../api";
+
+const CommentInput = styled(TextField)`
+  width: 100%;
+  background: #f0f2f5;
+  border: none;
+  outline: none;
+  border-radius: 20px;
+  margin-right: 5px;
+`;
+
+const SendButton = styled(SendRoundedIcon)`
+  color: #0264d3;
+  cursor: pointer;
+`;
+const SendButtonDisabled = styled(SendRoundedIcon)`
+  color: #bec3ca;
+  cursor: not-allowed;
+`;
+
+export default function CommentModal({ socket }) {
+  const [comments, setComments] = useState(null);
+  const [comment, setComment] = useState("");
+  const [arrivalComment, setArrivalComment] = useState(null);
+  const [isSendButtonDisabled, setIsSendButtonDisabled] = useState(true);
+
+  const scrollRef = useRef();
+
+  const { isShow, data } = useSelector(commentModalState$);
+  const post = data || {};
+  const user = useSelector(userState$);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    socket.on("receiveComment", (data) => {
+      setArrivalComment({
+        postId: data?.postId,
+        userId: data?.userId,
+        comment: data?.comment,
+        createdAt: Date.now(),
+      });
+    });
+
+    // Nếu có tin nhắn realtime thì thay đổi lại mảng comments
+    arrivalComment && setComments((prev) => [...prev, arrivalComment]);
+  }, [socket, arrivalComment]);
+
+  useEffect(() => {
+    // Lấy comments từ db
+    const fetchComments = async () => {
+      const comments = await api.fetchComments(post._id);
+      setComments(comments);
+    };
+
+    fetchComments();
+  }, [post._id]);
+
+  // Scroll xuống comment mới nhất
+  useEffect(() => {
+    scrollRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, [comments]);
+
+  const handleCloseModal = useCallback(() => {
+    dispatch(actions.hideCommentModal());
+  }, [dispatch]);
+
+  const handleCommentInputChange = (e) => {
+    const contentValue = e.target.value;
+    setComment(contentValue);
+    if (contentValue) {
+      setIsSendButtonDisabled(false);
+    } else {
+      setIsSendButtonDisabled(true);
+    }
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter" && !e.shiftKey) {
+      e.preventDefault(); // Ngăn xuống dòng khi chỉ nhấn Enter
+      handleSubmit();
+    }
+  };
+
+  const handleSubmit = async () => {
+    if (comment) {
+      const newComment = {
+        postId: post._id,
+        userId: user._id,
+        comment,
+      };
+
+      // Gửi tin nhắn lên socket
+      socket.emit("sendComment", newComment);
+      const res = await api.createComment(newComment);
+      setComments([...comments, res]);
+      setComment(""); // Xóa nội dung sau khi submit (nếu cần)
+      setIsSendButtonDisabled(true);
+    }
+  };
+
+  const body = (
+    <div className="comment-modal">
+      {/* Header */}
+      <div className="comment-modal--header">
+        Bài viết của {post?.author}
+        <div
+          className="comment-modal--header__close-button"
+          onClick={handleCloseModal}
+        >
+          <i className="fa-solid fa-xmark"></i>
+        </div>
+      </div>
+
+      {/* Current Post */}
+      <div className="comment-modal--post">
+        <Post post={post} dispatch={dispatch} />
+
+        {/* Read Comments */}
+        {comments?.map((comment) => (
+          <div key={comment._id} ref={scrollRef}>
+            <CommentItem key={comment._id} comment={comment} />
+          </div>
+        ))}
+      </div>
+
+      {/* Write Comment */}
+      <div className="comment-modal--write-comment">
+        <Avatar
+          sx={{
+            marginRight: "5px",
+          }}
+          src={user.avatar}
+        />
+        <CommentInput
+          placeholder="Viết bình luận"
+          multiline
+          minRows={1}
+          maxRows={10}
+          onChange={handleCommentInputChange}
+          onKeyDown={handleKeyDown}
+          value={comment}
+          slotProps={{
+            input: {
+              style: {
+                border: "none",
+                outline: "none",
+                borderRadius: "20px",
+                height: "40px",
+              },
+            },
+          }}
+        />
+        {isSendButtonDisabled ? (
+          <SendButtonDisabled />
+        ) : (
+          <SendButton onClick={handleSubmit} />
+        )}
+      </div>
+    </div>
+  );
+
+  return (
+    <Modal
+      open={isShow}
+      onClose={handleCloseModal}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      {body}
+    </Modal>
+  );
+}
